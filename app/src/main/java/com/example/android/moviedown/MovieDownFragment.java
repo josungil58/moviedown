47f4e2df8a069aac189c7a64ee5ff494e2ebaa36
josungil58 <josungil58@gmail.com>
2016-04-28 16:58:32 +0900

    1.00 initial input

---
+++ app/src/main/java/com/example/android/moviedown/MovieDownFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.moviedown;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieDownFragment extends Fragment {
+
+    public MovieDownFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public class FetchMovieTask extends AsyncTask<void, void, void>{
+
+        //getweatherdatafromjson에 해당하는 것을 coding해야 함함
+
+    }
+}
